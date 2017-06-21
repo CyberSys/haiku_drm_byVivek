f916a44ffb48f7f3bac1b8326895bc3bbef2c068
Vivek Roy <vivekroyandroid@gmail.com>
2017-06-22 04:45:25 +0530

    Add some freebsd compat headers

---
+++ headers/compatibility/freebsd/machine/endian.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2007 Haiku Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _FBSD_COMPAT_MACHINE_ENDIAN_H_
+#define _FBSD_COMPAT_MACHINE_ENDIAN_H_
+
+
+#include <freebsd/sys/cdefs.h>
+#include <freebsd/sys/_types.h>
+
+#endif
