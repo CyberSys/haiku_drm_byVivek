02607eb1978244594ccee2d4cabcf53454a87b1f
Vivek Roy <vivekroyandroid@gmail.com>
2017-07-17 01:02:24 +0530

    Add vm_inherit_t and vm_prot_t to vm.h

--- headers/compatibility/freebsd/vm/vm.h
+++ headers/compatibility/freebsd/vm/vm.h
@@ -20,6 +20,26 @@ typedef uint32_t vm_size_t;
 
 #endif
 
+typedef char vm_inherit_t;	/* inheritance codes */
+
+#define	VM_INHERIT_SHARE	((vm_inherit_t) 0)
+#define	VM_INHERIT_COPY		((vm_inherit_t) 1)
+#define	VM_INHERIT_NONE		((vm_inherit_t) 2)
+#define	VM_INHERIT_ZERO		((vm_inherit_t) 3)
+#define	VM_INHERIT_DEFAULT	VM_INHERIT_COPY
+
+typedef u_char vm_prot_t;	/* protection codes */
+
+#define	VM_PROT_NONE		((vm_prot_t) 0x00)
+#define	VM_PROT_READ		((vm_prot_t) 0x01)
+#define	VM_PROT_WRITE		((vm_prot_t) 0x02)
+#define	VM_PROT_EXECUTE		((vm_prot_t) 0x04)
+#define	VM_PROT_COPY		((vm_prot_t) 0x08)	/* copy-on-read */
+
+#define	VM_PROT_ALL		(VM_PROT_READ|VM_PROT_WRITE|VM_PROT_EXECUTE)
+#define VM_PROT_RW		(VM_PROT_READ|VM_PROT_WRITE)
+#define	VM_PROT_DEFAULT		VM_PROT_ALL
+
 typedef page_num_t vm_pindex_t;
 typedef void *pmap_t;
 typedef addr_t vm_offset_t;
